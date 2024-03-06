dd00bd0b484e730fa8fb5ba5cc9045ce483b97ea
Tomáš <156953640+PrejdaTomas@users.noreply.github.com>
2024-03-06 21:58:43 +0000

    Update main.c
    velký refactor
    
    1. všechny printy změněny na wrapper, který přijímá argumenty a pomocí fflush vyčistí paměť
    2. wrappery mají další wrappery (tail, mid, head) - ty tisknou trochu poměněný output o tabulátory, apod.
    3. leccos začištěno
    4. otestovat v práci

--- main.c
+++ main.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <math.h>
 
 
-#define BUF_SIZE 65536
-#define BUF_SIZE_STRING 512
+#define BUF_SIZE 65536 //65536
+#define BUF_SIZE_STRING 512 //512
+#define ITER_PRINT_FREQUENCY 40
+
+void fprintfWrapper_newline(const char* format, ...);
+void fprintfWrapper_newline_head(const char* format, ...);
+void fprintfWrapper_newline_mid(const char* format, ...);
+void fprintfWrapper_newline_tail(const char* format, ...);
+void fprintfWrapper_comma(const char* format, ...);
 
 
 void swap(double* a, double* b);
 int partition(double arr[], int low, int high);
 void quickSort(double arr[], int low, int high);
 
-void swapbsort(int* xp, int* yp);
-void bubbleSort(int *arr, int n);
-
-
 double *arrayGen1D(int count);
 double **arrayGen2D(int countRow, int countCol);
 double **getRectangle(int countRow, int countCol, int xIndex, int yIndex, double **arrayIpt);
-double **readFromCSV(char *path);
+double **readFromCSV(char *filePath);
 
 double interpolation2D_bilinear(int countRow, int countCol, double **pointset, double x, double y, int selectedColumn);
 
-int countLinesFile(char *path);
-int countLinesFile_noHeader(char *path);
+int countLinesFile(char *filePath);
+int countLinesFile_noHeader(char *filePath);
 int countLinesReadTable(double **arrayIpt);
-int countColumnsFile(char *path);
+int countColumnsFile(char *filePath);
 int countColumnsReadTable(double **arrayIpt);
 int findClosest(double temperature, double pressure, double **arrayIpt);
 
@@ -36,53 +39,138 @@ void printArray1D(int count, double *arrayIpt);
 void printArray2D(int countRow, int countCol, double **arrayIpt);
 void newline(int count);
 
+/*
 void sin2D(int countRow, int countCol, double **arrayIpt);
+*/
+
 char *strConcat(const char *s1, const char *s2);
 
 double *extractColumnFromTable(int index, double **arrayIpt);
 double *createUniqueValuesArray(int countRow, double *arrayIpt);
 
+bool debug = true;
+bool debugMain = true;
 
 int main()
 {
-	newline(2);
-	char *tablePath		= "table.csv";
-	int row_count_csv	= countLinesFile_noHeader(tablePath);
-	int column_count_csv	= countColumnsFile(tablePath);
+    if (debugMain)
+    {
+	    newline(2);   
+        printf("Running the Main!\n");
+        fflush(stdout); /*branim se preteceni stdout bufferu a sigsegv*/
+    }
 
-	double **read		= readFromCSV(tablePath);
-	printArray2D(row_count_csv, column_count_csv, read);
-	newline(1);
-	int row_count_array	= countLinesReadTable(read);
+	char *tablefilePath		= "table.csv";
+	int row_count_csv	    = countLinesFile_noHeader(tablefilePath);
+	int column_count_csv	= countColumnsFile(tablefilePath);
+
+	double **read		    = readFromCSV(tablefilePath);
+    if (debugMain)
+    {
+        printf("Finished reading the csv in the main loop!\n");
+	    printArray2D(row_count_csv, column_count_csv, read);
+        newline(1);
+        fflush(stdout);
+    }
+    //getchar();
+	int row_count_array	    = countLinesReadTable(read);
 	int column_count_array	= countColumnsReadTable(read);
-	newline(2);
 
+	if (debugMain) {newline(2);}
+
+    if (debugMain) {printf("TEMPERATURES\n");}
 	double *temperatures	= extractColumnFromTable(0, read);
-	double *pressures	= extractColumnFromTable(1, read);
-	printArray1D(row_count_array, pressures);
-	printArray1D(row_count_array, temperatures);
+    
+    if (debugMain) {printf("PRESSURES\n");}
+	double *pressures       = extractColumnFromTable(1, read);
 
+    if (debugMain)
+    {
+        printArray1D(row_count_array, pressures);
+        printArray1D(row_count_array, temperatures);
+        fflush(stdout);
+    }
 	quickSort(temperatures, 0, row_count_array -1 ); 
 	quickSort(pressures, 0, row_count_array -1 ); 
 
 	//bubbleSort(temperatures, row_count_array);
 	//bubbleSort(pressures, row_count_array);
-	printArray1D(row_count_array, pressures);
-	printArray1D(row_count_array, temperatures);
+    if (debugMain)
+    {
+	    printArray1D(row_count_array, pressures);
+        fflush(stdout);
+	    printArray1D(row_count_array, temperatures);
+        fflush(stdout);
+    }
 
 	double *unique_temperatures	= createUniqueValuesArray(row_count_array, temperatures);
 	double *unique_pressures	= createUniqueValuesArray(row_count_array, pressures);
 
-
+    free(temperatures);
+    free(pressures);
 	double **rect	= getRectangle(row_count_csv, column_count_csv, 20, 3, read);
-	printArray2D(4, 2, rect);
-	newline(2);
+	if (debugMain)
+    {
+        printArray2D(4, 2, rect);
+	    newline(2);
+        fflush(stdout);
+    }
 
 
 	return 1;
 }
 
 
+void fprintfWrapper_newline(const char* format, ...)
+{
+    if (debug)
+    {
+        va_list args;
+        va_start(args, format);
+        vfprintf(stdout, format, args);
+        fprintf(stdout, "\n");  // Print to stdout (you can change this)
+        fflush(stdout);
+        va_end(args);
+    }
+}
+
+void fprintfWrapper_newline_tail(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    fprintf(stdout, "\t");
+    fprintfWrapper_newline(format, args);
+    fprintfWrapper_newline("", args);
+}
+
+void fprintfWrapper_newline_mid(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    fprintf(stdout, "\t");
+    fprintfWrapper_newline(format, args);
+}
+
+
+void fprintfWrapper_newline_head(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    fprintfWrapper_newline(format, args);
+}
+
+void fprintfWrapper_comma(const char* format, ...)
+{
+    if (debug)
+    {
+        va_list args;
+        va_start(args, format);
+        vfprintf(stdout, format, args);
+        fprintf(stdout, ", ");  // Print to stdout (you can change this)
+        fflush(stdout);
+        va_end(args);
+    }
+}
 
 
 
@@ -123,31 +211,6 @@ void quickSort(double arr[], int low, int high)
  
 
 
-// An optimized version of Bubble Sort
-void bubbleSort(int *arr, int n)
-{
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
-            }
-        }
- 
-        // If no two elements were swapped by inner loop,
-        // then break
-        if (swapped == false)
-            break;
-    }
-}
-
-
-
-
-
 
 
 
@@ -165,22 +228,23 @@ void bubbleSort(int *arr, int n)
 
 double *arrayGen1D(int count)
 {
-	printf("Generating 1D array!\n");
+    fprintfWrapper_newline_head("arrayGen1D: start!");
 	double *nuArray = (double*)malloc(sizeof(double)*count);
 	int index;
 	for (index = 0; index < count; index++)
 	{
 		nuArray[index] = (double)index;
 	}
+    fprintfWrapper_newline_mid("arrayGen1D: success!");
 	return nuArray;
 }
 
 double **arrayGen2D(int countRow, int countCol)
 {
-	printf("Generating 2D array!\n");
+	fprintfWrapper_newline_head("arrayGen2D: start!");
 	double **nuArray = (double**)malloc(sizeof(double*)*countRow);
-	int indexRow;
-	int indexCol;
+	int indexRow, indexCol;
+    
 	for (indexRow = 0; indexRow < countRow; indexRow++)
 	{
 		nuArray[indexRow] = (double*)malloc(sizeof(double)*countCol);
@@ -189,6 +253,7 @@ double **arrayGen2D(int countRow, int countCol)
 			nuArray[indexRow][indexCol] = (double)(indexRow+indexCol)/10;
 		}
 	}
+    fprintfWrapper_newline_tail("arrayGen2D: success!");
 	return nuArray;
 }
 
@@ -196,7 +261,7 @@ double **arrayGen2D(int countRow, int countCol)
 
 void printArray1D(int count, double *arrayIpt)
 {
-	printf("Printing 1D array!\n");
+	fprintfWrapper_newline_head("printArray1D: start!");
 	int index;
 	printf("[");
 	for (index = 0; index < count; index++)
@@ -204,13 +269,14 @@ void printArray1D(int count, double *arrayIpt)
 		printf("%lf, ", arrayIpt[index]);
 	}
 	printf("]\n");
+    fflush(stdout);
+    fprintfWrapper_newline_tail("printArray1D: success!");
 }
 
 void printArray2D(int countRow, int countCol, double **arrayIpt)
 {
-	printf("Printing 2D array!\n");
-	int indexRow;
-	int indexCol;
+	fprintfWrapper_newline_head("printArray2D: start!");
+	int indexRow, indexCol;
 	printf("[\n");
 	for (indexRow = 0; indexRow < countRow; indexRow++)
 	{
@@ -222,13 +288,16 @@ void printArray2D(int countRow, int countCol, double **arrayIpt)
 		printf("]\n");
 	}
 	printf("]\n");
+    fprintfWrapper_newline_tail("printfArray2D: success!");
+    fflush(stdout);
 }
 
+
+/*
 void sin2D(int countRow, int countCol, double **arrayIpt)
 {
-	printf("Sinus 2D array!\n");
-	int indexRow;
-	int indexCol;
+	fprintfWrapper_newline_head("sin2D: start!");
+	int indexRow, indexCol;
 	for (indexRow = 0; indexRow < countRow; indexRow++)
 	{
 		for (indexCol = 2; indexCol < countCol; indexCol++)
@@ -236,7 +305,11 @@ void sin2D(int countRow, int countCol, double **arrayIpt)
 			arrayIpt[indexRow][indexCol] = sin(arrayIpt[indexRow][indexCol]/180.0*3.14159);
 		}
 	}
+    fprintfWrapper_newline_tail("sin2D: success!");
+
 }
+*/
+
 void newline(int count)
 {
 	int position = 0;
@@ -246,9 +319,10 @@ void newline(int count)
 	}
 	while (position < count) ;
 }
+
 double interpolation2D_bilinear(int countRow, int countCol, double **pointset, double x, double y, int selectedColumn)
 {
-	printf("Bilinear interpolation\n");
+	fprintfWrapper_newline_head("interpolation2D_bilinear: start");
 	double result;
 	int indexRow;
 	int indexCol;
@@ -275,13 +349,14 @@ double interpolation2D_bilinear(int countRow, int countCol, double **pointset, d
 	double rb = q22 * (x - x1) * (y - y1);
 	
 	result = (lt + rt + lb + rb)/dxdy;
+    fprintfWrapper_newline_mid("interpolation2D_bilinear: success!");
 	return result;
 }
 
 
 double *createUniqueValuesArray(int countRow, double *arrayIpt)
 {
-	printf("Creating Unique Values Array!\n");
+	fprintfWrapper_newline_head("createUniqueValuesArray: start!");
 	int uniqueVals  = 1;
 	int position_1  = 0;
 	int position_2  = 0;
@@ -291,71 +366,74 @@ double *createUniqueValuesArray(int countRow, double *arrayIpt)
 		uniqueVals++;
 		position_1++;
 	}
-	printf("\tReached the next unique value! Breaking at %d unique values!\n", uniqueVals);
+	fprintfWrapper_newline_mid("createUniqueValuesArray: reached unique value -> Breaking at %d unique values!", uniqueVals);
 	
 
 	double *nuArray = (double*)malloc(sizeof(double)*uniqueVals);
-	printf("\tCreating a 1D array of unique values!\n");
+	fprintfWrapper_newline_mid("createUniqueValuesArray: creating unique 1D array!");
 	for (position_1 = 0; position_1 < countRow; position_1++)
 	{
 		if (arrayIpt[position_1] != nuArray[position_2])
 		{
 			nuArray[position_2] = arrayIpt[position_1];
 			position_2++;
-			printf("\t\tAdded %lf to arrayIpt at position %d!\n", arrayIpt[position_1], position_2);
+			fprintfWrapper_newline_mid("\tAdded %lf to arrayIpt at position %d!", arrayIpt[position_1], position_2);
 		}
 	}
-	printf("\tSuccesfully created the unique values array!\n");
+	fprintfWrapper_newline_tail("createUniqueValuesArray: success!");
 	return nuArray;
 }
 
 
 int countLinesReadTable(double **arrayIpt)
 {
-	printf("Counting lines from a read table!\n");
+	fprintfWrapper_newline_head("countLinesReadTable: start!");
 	int position = 0;
 	while (arrayIpt[position])
 	{
 		position++;
 	}
-	printf("\tSuccess: The table has: %d lines!\n", position);
+	fprintfWrapper_newline_tail("countLinesReadTable: success -> %d lines!", position);
 	return position;
 }
 
 
 int countColumnsReadTable(double **arrayIpt)
 {
-	printf("Counting columns from a read table!\n");
+	fprintfWrapper_newline_head("countColumnsReadTable: start!");
 	int position = 0;
 	while (arrayIpt[0][position])
 	{
 		position++;
 	}
-	printf("\tSuccess: The table has: %d columns!\n", position);
+	fprintfWrapper_newline_tail("countColumnsReadTable: success -> %d columns!", position);
 	return position;
 }
 
 
 double *extractColumnFromTable(int index, double **arrayIpt)
 {
-	printf("Extracting column from a read table!\n");
+	fprintfWrapper_newline_head("extractColumnFromTable: start!");
 	int position;
-	printf("\tExtracting column: getting the numLines!\n");
+	fprintfWrapper_newline_mid("extractColumnFromTable: getting the numLines!");
+
 	int numLines	= countLinesReadTable(arrayIpt);
-	printf("\tExtracted column will have %d lines!\n", numLines);
+	fprintfWrapper_newline_mid("extractColumnFromTable: extracted column will have %d lines!", numLines);
+
 	double *nuArray = (double*)malloc(sizeof(double)*numLines);
 
 	for (position = 0; position < numLines; position++)
 	{
 		nuArray[position] = arrayIpt[position][index];
 	}
-	printf("\tSuccesfully extracted!\n");
+	fprintfWrapper_newline_tail("extractColumnFromTable: success!");
 	return nuArray;
 }
 
 int findClosest(double temperature, double pressure, double **arrayIpt)
 {
-	printf("Finding the closest (T,P) value!\n");
+	fprintfWrapper_newline_head("findClosest: start -> T/P [%lf K, %lf MPa]", temperature, pressure);
+	fprintfWrapper_newline_tail("findClosest: success!");
 	return -1;
 }
 
@@ -363,7 +441,7 @@ int findClosest(double temperature, double pressure, double **arrayIpt)
 
 double **getRectangle(int countRow, int countCol, int xIndex, int yIndex, double **arrayIpt)
 {
-	printf("Generating a rectangle around [%d , %d]!\n", xIndex, yIndex);
+	fprintfWrapper_newline_head("getRectangle: start -> indices[%d , %d]!", xIndex, yIndex);
 	double **nuArray = (double**)malloc(sizeof(double*)*4);
 	
 	nuArray[0] = (double*)malloc(sizeof(double)*countCol);
@@ -375,7 +453,7 @@ double **getRectangle(int countRow, int countCol, int xIndex, int yIndex, double
 	nuArray[1] = arrayIpt[xIndex+1];
 	nuArray[2] = arrayIpt[xIndex+0];
 	nuArray[3] = arrayIpt[xIndex+1];
-		
+	fprintfWrapper_newline_tail("getRectangle: success!");
 	return nuArray;
 }
 
@@ -388,18 +466,17 @@ char* strConcat(const char *s1, const char *s2)
 
 	memcpy(result, s1, len1);
 	memcpy(result + len1, s2, len2 + 1);
-	printf("\tSuccesfully interpolated!\n");
 	return result;
 }
 
 
-int countLinesFile(char *path)
+int countLinesFile(char *filePath)
 {
-	printf("Counting lines from: %s!\n", path); 
-	FILE *file = fopen(path, "r");
+	fprintfWrapper_newline_head("countLinesFile: start -> from %s!", filePath);
+	FILE *file = fopen(filePath, "r");
 	if (file != NULL)
 	{
-		printf("Reading %s\n", path);
+		fprintfWrapper_newline_mid("countLinesFile: reading from %s", filePath);
 		char buf[BUF_SIZE];
 		int counter = 0;
 		for(;;)
@@ -409,33 +486,35 @@ int countLinesFile(char *path)
 
 				int i;
 				for(i = 0; i < res; i++)
-			{
-				if (buf[i] == '\n') {counter++ ;}
-			}
-
+                {
+                    if (buf[i] == '\n') {counter++ ;}
+                }
 				if (feof(file)) {break; }
 		}
+        fprintfWrapper_newline_mid("countLinesFile: closing %s", filePath);
 		fclose(file);
-		printf("\tSuccess: The number of lines in %s:\t%d\n", path, counter);
+
+		fprintfWrapper_newline_tail("countLinesFile: success -> lines in %s: %d", filePath, counter);
 		return counter;
 	}
-	printf("\tThe file does not exist: %s\n", path);
+	fprintfWrapper_newline_tail("countLinesFile: failure -> not exist:  %s", filePath);
 	return -1;
 }
 
-int countLinesFile_noHeader(char *path)
+int countLinesFile_noHeader(char *filePath)
 {
-	printf("Counting lines from %s without a header!\n", path);
-	return countLinesFile(path) - 1;
+	fprintfWrapper_newline_head("countLinesFile_noHeader: start -> from %s without a header!", filePath);
+	fprintfWrapper_newline_tail("countLinesFile_noHeader: success");
+	return countLinesFile(filePath) - 1;
 }
 
-int countColumnsFile(char *path)
+int countColumnsFile(char *filePath)
 {
-	printf("Counting columns from %s!\n", path);
-	FILE *file = fopen(path, "r");
+	fprintfWrapper_newline_head("countColumnsFile: start -> from %s!", filePath);
+	FILE *file = fopen(filePath, "r");
 	if (file != NULL)
 	{
-		printf("Reading %s\n", path);
+		fprintfWrapper_newline_mid("countColumnsFile: reading %s", filePath);
 		char buf[BUF_SIZE];
 		int counter = 0;
 		for(;;)
@@ -452,23 +531,25 @@ int countColumnsFile(char *path)
 
 				if (feof(file)) {break; }
 		}
+
 		fclose(file);
+
 		if (counter > 1) {counter++;}
-		printf("\tSuccess: The number of columns in %s:\t%d\n", path, counter);
+		fprintfWrapper_newline_tail("countColumnsFile: success -> columns in %s: %d", filePath, counter);
 		return counter;
 	}
-	printf("\tThe file does not exist: %s\n", path);
+	fprintfWrapper_newline_tail("countColumnsFile: failure -> does not exist: %s", filePath);
 	return -1;
 }
 
-double **readFromCSV(char *path)
+double **readFromCSV(char *filePath)
 {
-	printf("Reading a table from a csv: %s!\n", path);
+	fprintfWrapper_newline_head("readFromCSV: start -> from %s!", filePath);
 	double stringToNumber;
 	int position	= 0;
-	int lineNum	 = 0;
-	int rowCount 	= countLinesFile_noHeader(path);
-	int columnCount = countColumnsFile(path);
+	int lineNum	    = 0;
+	int rowCount 	= countLinesFile_noHeader(filePath);
+	int columnCount = countColumnsFile(filePath);
 
 	char *headers[32]; 
 	char line[BUF_SIZE_STRING];
@@ -480,42 +561,62 @@ double **readFromCSV(char *path)
 		nuArray[position] = (double*)malloc(sizeof(double)*columnCount);
 	}
 
-	FILE* file = fopen(path, "r");
+	FILE* file = fopen(filePath, "r");
 	if (file != NULL)
 	{
-		printf("Reading %s\n", path);
+		fprintfWrapper_newline_mid("readFromCSV: reading %s", filePath);
 		while (fgets(line, sizeof(line), file))
 		{
 			if (line[0] == '\n') {continue;}
 			position = 0;
-			newline(1);
+
 			data = strtok(line, ", ");
 			while (data != NULL)
 			{
 				sscanf(data, "%lf", &stringToNumber); /* na adresu d prevedu hodnotu ze stringu na double */
-
+                //free(data);
 				if (lineNum == 0)
 				{
 					headers[position] = strdup(data); /* aby mi zustala data, porad odkazuji pointery, tak musim string v headeru duplikovat */
-					printf("%s, ", headers[position]);
+					fprintfWrapper_comma("%s", headers[position]);
 				}
 
-				if (lineNum > 0)
+				if (lineNum > 0  && lineNum < (rowCount*columnCount))
 				{
 					/*printf("%s: %.3lf, ", headers[position], stringToNumber);*/
-					printf("%.6lf, ", stringToNumber);
+
+                    if (lineNum % ITER_PRINT_FREQUENCY == 0)
+                    {
+                        fprintfWrapper_comma("%.6lf", stringToNumber);
+                    }
 					nuArray[lineNum-1][position] = stringToNumber;
 				}
 				data = strtok(NULL, ", ");
+                fflush(stdout);
 				position++;
 			}
+
+            if (lineNum % ITER_PRINT_FREQUENCY == 0) {fprintfWrapper_newline("");}
+            if (lineNum > rowCount*columnCount)
+            {
+                fprintfWrapper_newline_mid("readFromCSV: Exceeded the length of a file, how?");
+                break;
+            }
 			lineNum++;
+            free(line);
 		}
-		newline(1);
+		//if (lineNum < 50)   {newline(1);}
+        if (lineNum == ITER_PRINT_FREQUENCY)  {fprintfWrapper_newline_mid("readFromCSV: End of table preview!");}
+
+        fprintfWrapper_newline_mid("readFromCSV: closing %s", filePath);
 		fclose(file);
-		printf("\tSuccesfully read the table into a 2D array!\n");
+		fprintfWrapper_newline_tail("readFromCSV: success -> read %s into a 2D array!", filePath);
 		return nuArray;
-	}
-	printf("\tThe file does not exist: %s\n", path);
+    }
+    
+    fprintfWrapper_newline_tail("readFromCSV: failure -> does not exist: %s", filePath);
 	return NULL;
 }
+
+
+
